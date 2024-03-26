2d07e570e423447b78680c7bbbd982660770e348
CptFlashbang~ <Callum.yates.180@gmail.com>
2024-03-26 10:25:31 +0000

    wrapped all images inside coloured containers

--- lib/pages/about.dart
+++ lib/pages/about.dart
@@ -24,12 +24,31 @@ class _AboutPageState extends State<AboutPage> {
               color: const Color.fromARGB(255, 18, 45, 200),
               child: const Image(image: AssetImage('images/cat1.jpg')),
             ),
-
-            Expanded(child: Image(image: AssetImage('images/cat2.jpg'))),
-            Expanded(child: Image(image: AssetImage('images/cat3.jpg'))),
-            Expanded(child: Image(image: AssetImage('images/cat4.jpg'))),
-            Expanded(child: Image(image: AssetImage('images/cat5.jpg'))),
-            Expanded(child: Image(image: AssetImage('images/cat6.jpg'))),
+            Container(
+              height: 200,
+              color: Color.fromARGB(255, 200, 18, 18),
+              child: const Image(image: AssetImage('images/cat2.jpg')),
+            ),
+            Container(
+              height: 200,
+              color: Color.fromARGB(255, 255, 102, 0),
+              child: const Image(image: AssetImage('images/cat3.jpg')),
+            ),
+            Container(
+              height: 200,
+              color: Color.fromARGB(255, 255, 251, 0),
+              child: const Image(image: AssetImage('images/cat4.jpg')),
+            ),
+            Container(
+              height: 200,
+              color: Color.fromARGB(255, 21, 255, 0),
+              child: const Image(image: AssetImage('images/cat5.jpg')),
+            ),
+            Container(
+              height: 200,
+              color: Color.fromARGB(255, 0, 195, 255),
+              child: const Image(image: AssetImage('images/cat6.jpg')),
+            ),
           ],
         ),
 
