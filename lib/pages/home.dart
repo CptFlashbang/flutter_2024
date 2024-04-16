9baf116582ad2e9d67d3ea801f8adfb34d14bc9c
CptFlashbang~ <Callum.yates.180@gmail.com>
2024-04-16 11:24:28 +0100

    changed child to use media query modifiers

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -21,12 +21,28 @@ class _HomePageState extends State<HomePage> {
       ),
       body: Container(
         margin: const EdgeInsets.all(10.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: const <Widget>[
-            Text("Home"),
+        child: ListView(
+          children: <Widget>[
+            Text('Height: ${media.size.height}'),
+            Text('Width: ${media.size.width}'),
+            Text('Device Pixel Ratio: ${media.devicePixelRatio}'),
+            Text('Scale Factor: ${MediaQuery.textScaleFactorOf(context)}'),
+            Text('Brightness: ${MediaQuery.platformBrightnessOf(context)}'),
+            Text('System View Insets: ${media.viewInsets}'),
+            Text('System Padding: ${media.padding}'),
+            Text('System View Padding: ${media.viewPadding}'),
+            Text('System Gesture Insets: ${media.systemGestureInsets}'),
+            Text('Always 24 Hours: ${media.alwaysUse24HourFormat}'),
+            Text('Accessible Navigation: ${media.accessibleNavigation}'),
+            Text('Inverting Colors: ${media.invertColors}'),
+            Text('In High Contrast: ${MediaQuery.highContrastOf(context)}'),
+            Text('Disable Animation: ${media.disableAnimations}'),
+            Text('In Bold Text: ${MediaQuery.boldTextOverride(context)}'),
+            Text('Navigation Mode: ${media.navigationMode}'),
+            Text('Orientation: ${media.orientation}'),
           ],
+        ),
+
         ),
       ),
     );
